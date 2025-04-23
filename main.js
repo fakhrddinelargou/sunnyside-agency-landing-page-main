0730ef0aceca1ec6a3086bb608ef32fd50f7a9e2
fakhreddine <largofakhrddin@gmail.com>
2025-04-23 15:12:18 +0100

    feat: implement layout design and add responsive styles for mobile

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+// const fill = document.querySelectorAll('.fillColor')
+
+
+
+
+
+// const allFill =Array.from(fill)
+
+// console.log(allFill);
+
+
+// allFill.forEach((even) =>{
+
+//     even.addEventListener('mouseleave' , ()=>
+
+//         even.setAttribute('fill' , 'green')
+  
+//     );
+
+// })
